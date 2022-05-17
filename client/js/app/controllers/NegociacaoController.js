975685a75a351d9448ed8d7c073907371c534eb9
Bruno Rafael Filgueira Dias <brunor_17@hotmail.com>
2022-05-17 15:34:30 -0300

    Implementando o fluxo para adicionando a tabela, e o fluxo para criar uma nova negociação

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,19 +5,35 @@ class NegociacaoController {
     this._inputData = $('#data');
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
+    this._listaNegociacoes = new ListaNegociacoes();
+
+    this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+    this._negociacoesView.update(this._listaNegociacoes);
   }
 
   adicionar(event) {
     event.preventDefault();
 
-    let negociacao = new Negociacao(
-      new Date(...this._inputData.value
-        .split('-')
-        .map((item, indice) => item - indice % 2)),
+    let negociacao = this._criaNegociacao();
+
+    this._listaNegociacoes.adicionar(negociacao);
+    this._negociacoesView.update(this._listaNegociacoes);
+    this._limparFormulario();
+  }
+
+  _criaNegociacao() {
+    return new Negociacao(
+      DateHelper.textoParaData(this._inputData.value),
       this._inputQuantidade.value,
       this._inputValor.value
     );
+  }
 
-    console.log(negociacao);
+  _limparFormulario() {
+    this._inputData.value = '';
+    this._inputQuantidade.value = 1;
+    this._inputValor.value = 0.0;
+
+    this._inputData.focus();
   }
 }
