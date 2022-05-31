6581168558552680b5c51542dcb36b4a8437b774
BrunnoRafa <brunor_17@hotmail.com>
2022-05-31 10:25:20 -0300

    Criando método para apagar a lista de negociações

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -44,4 +44,13 @@ class NegociacaoController {
 
     this._inputData.focus();
   }
+
+  apaga() {
+
+    this._listaNegociacoes.esvazia();
+    this._negociacoesView.update(this._listaNegociacoes);
+
+    this._mensagem.texto = 'Negociações apagadas com sucesso';
+    this._mensagemView.update(this._mensagem);
+}
 }
