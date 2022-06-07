dfde439dac3d23c0ce1842820f60b4261aa0dc16
BrunnoRafa <brunor_17@hotmail.com>
2022-06-07 15:40:08 -0300

    Implementando novos métodos na classe ListaNegociacoes

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -7,11 +7,23 @@ class ListaNegociacoes {
     this._listaNegociacoes.push(negociacao);
   }
 
-  esvazia()   {
+  esvazia() {
     this._listaNegociacoes = [];
+  }
+
+  ordena(criterio) {
+    this._listaNegociacoes.sort(criterio);
+  }
+
+  inverteOrdem() {
+    this._listaNegociacoes.reverse();
   }
 
   get negociacoes() {
     return [].concat(this._listaNegociacoes);
+  }
+
+  get volumeTotal() {
+    return this._listaNegociacoes.reduce((total, n) => total + n.volume, 0.0);
   }
 }
